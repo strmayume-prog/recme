3b06dd6a14f716819805a4bbe6a888cb969a2ef7
strmayume-prog <str.mayume@gmail.com>
2025-11-06 23:14:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ logging.basicConfig(
 
 # Get bot token
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '7470899134:AAHAukDv6b1CKadBYv9rwEP5P3oECCgjymo')
-PAYMENT_LINK = "https://buy.stripe.com/eVqeV5as37G4bbD1YZgEg01"
+PAYMENT_LINK_USD = "https://buy.stripe.com/eVqeV5as37G4bbD1YZgEg01"
+PAYMENT_LINK_BR = "https://buy.stripe.com/3cI7sDdEf3pO1B3bzzgEg02"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     
-    keyboard = [[InlineKeyboardButton("USD 17.99", url=PAYMENT_LINK)]]
+    keyboard = [[InlineKeyboardButton("USD 17.99", url=PAYMENT_LINK_USD)]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+     keyboard = [[InlineKeyboardButton("BRL 0,50", url=PAYMENT_LINK_BR)]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     
     message_text = """Attention: Follow the instructions
